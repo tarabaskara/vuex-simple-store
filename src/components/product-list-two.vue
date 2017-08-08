1e77eb8db6d9a168f4318aa7b3c66a2984655daa
Tara Baskara <tbaskara@starqle.com>
2017-08-08 15:35:51 +0700

    Added mutations

--- src/components/product-list-two.vue
+++ src/components/product-list-two.vue
@@ -7,6 +7,7 @@
         <span class="price">${{product.price}}</span>
       </li>
     </ul>
+    <button v-on:click="reducePrice">Reduce Price</button>
   </div>
 </template>
 
@@ -19,6 +20,11 @@ export default {
     },
     saleProducts(){
       return this.$store.getters.saleProducts;
+    }
+  },
+  methods: {
+    reducePrice: function(){
+      this.$store.commit('reducePrice');
     }
   }
 }
