c0e4e8c72dead1ec18f216af666d9013301cf70b
Tara Baskara <tbaskara@starqle.com>
2017-08-08 13:57:24 +0700

    Added getters

--- src/components/product-list-one.vue
+++ src/components/product-list-one.vue
@@ -2,7 +2,7 @@
   <div id="product-list-one">
     <h2>Product List One</h2>
     <ul>
-      <li v-for="product in products">
+      <li v-for="product in saleProducts">
         <span class="name">{{product.name}}</span>
         <span class="price">${{product.price}}</span>
       </li>
@@ -16,6 +16,9 @@ export default {
   computed: {
     products(){
       return this.$store.state.products
+    },
+    saleProducts(){
+      return this.$store.getters.saleProducts
     }
   }
 }
--- src/components/product-list-two.vue
+++ src/components/product-list-two.vue
@@ -2,7 +2,7 @@
   <div id="product-list-two">
     <h2>Product List Two</h2>
     <ul>
-      <li v-for="product in products">
+      <li v-for="product in saleProducts">
         <span class="name">{{product.name}}</span>
         <span class="price">${{product.price}}</span>
       </li>
@@ -16,6 +16,9 @@ export default {
   computed: {
     products(){
       return this.$store.state.products
+    },
+    saleProducts(){
+      return this.$store.getters.saleProducts;
     }
   }
 }
