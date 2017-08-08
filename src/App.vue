a7ece592418822d258bce66dabd75b869dfd6b36
Tara Baskara <tbaskara@starqle.com>
2017-08-08 12:50:55 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/product-list-one.vue'
+import ProductListTwo from './components/product-list-two.vue'
+
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      products: [
+          {name: "Banana Skin", price: 20},
+          {name: "Shiny Star", price: 40},
+          {name: "Green Shells", price: 60},
+          {name: "Red Shells", price: 80},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/product-list-one.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="product in products">
+        <span class="name">{{product.name}}</span>
+        <span class="price">${{product.price}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['products'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
